5cdf5af82da3cf6820ee677c33351d3063059085
tensept <vankaew58@gmail.com>
2022-12-23 15:55:10 +0700

    lab9_3 compeleted

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 using namespace std;
-
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+int main(){
+    int age;
+    float height;
+    long int bounty;
+    string name;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+         cout << "Enter your height: ";
+         cin >> height;
+         if(height < 100 ){
+            name = "Chopper";
+         }
+         else if(height < 180){
+            name = "Usopp";
+         }
+         else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 1100000000){
+                name = "Zoro";
+            }
+            else{
+                name = "Sanji";
+            }
+         }
+    }
+    else if(age <= 60 ){
+         cout << "Enter your bounty: ";
+         cin >> bounty;
+         if(bounty > 500000000){
+            name = "Jinbe";
+         }
+         else{
+            name = "Franky";
+         }
+    }
+    else{
+        name = "Brook";
+    }
+    cout << "Your character = " << name;
+    
+}
